95505a96ac097bae710eae50ca9d8d6b1cdfddbb
bestjinam <ggul_jam@icloud.com>
2024-01-04 20:34:46 +0900

    몰루

---
+++ shopping-cart-with-vanillaJS/starter/frontend/static/js/helper/ProductsHelper.js
@@ -0,0 +1,9 @@
+export default class ProductHelper {
+    static get getProducts() {
+        return JSON.parse(sessionStorage.getItem('products'));
+    }
+
+    static set setProducts(products) {
+        sessionStorage.setItem('products', JSON.stringify(products));
+    }
+}
